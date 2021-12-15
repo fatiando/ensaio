5cc51015195d063b3dfecb2cb0f7a3ef377c5c2d
Leonardo Uieda <leouieda@gmail.com>
2021-12-15 09:21:57 +0000

    Fix package metadata and add deploy Action
    The license information was wrong and moved the setuptools_scm
    configuration to pyproject.toml. Add the GitHub Actions workflow for
    PyPI and TestPyPI deployment.

--- setup.py
+++ setup.py
@@ -5,19 +5,11 @@
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
 """
-Setup configuration for the Python package.
-
+Setup script for the Python package.
 Metadata and build configuration are defined in setup.cfg
 Uses setuptools-scm to manage version numbers using git tags.
 """
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(
-        use_scm_version={
-            "relative_to": __file__,
-            "version_scheme": "post-release",
-            "local_scheme": "node-and-date",
-            "write_to": "ensaio/_version_generated.py",
-        }
-    )
+    setup()
