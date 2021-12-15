5bf2d53df74b091866af7b6cd31ba2a71a871043
Leonardo Uieda <leouieda@gmail.com>
2021-12-15 14:16:39 +0000

    Allow setting the URL for the data source
    Create the Pooch instance in a function so that we can set the URL based
    on an environment variable. This would allow changing from Zenodo to
    figshare or GitHub if desired.

--- ensaio/v1.py
+++ ensaio/v1.py
@@ -4,33 +4,58 @@
 #
 # This code is part of the Fatiando a Terra project (https://www.fatiando.org)
 #
+import os
 from pathlib import Path
 
 import pooch
 
 DOI = "10.5281/zenodo.5167357"
 
-_REPOSITORY = pooch.create(
-    path=Path(pooch.os_cache("ensaio")) / "v1",
-    base_url=f"doi:{DOI}/",
-    registry={
-        "alps-gps-velocity.csv.xz": "md5:195ee3d88783ce01b6190c2af89f2b14",
-        "britain-magnetic.csv.xz": "md5:8dbbda02c7e74f63adc461909358f056",
-        "british-columbia-lidar.csv.xz": "md5:354c725a95036bd8340bc14e043ece5a",
-        "caribbean-bathymetry.csv.xz": "md5:a7332aa6e69c77d49d7fb54b764caa82",
-        "earth-geoid-10arcmin.nc": "md5:39b97344e704eb68fa381df2eb47da0f",
-        "earth-gravity-10arcmin.nc": "md5:56df20e0e67e28ebe4739a2f0357c4a6",
-        "earth-topography-10arcmin.nc": "md5:c43b61322e03669c4313ba3d9a58028d",
-        "southern-africa-gravity.csv.xz": "md5:1dee324a14e647855366d6eb01a1ef35",
-    },
-    # The name of an environment variable that can overwrite the path
-    env="ENSAIO_V1_DATA_DIR",
-)
+
+def _make_repository():
+    """
+    Create the :class:`pooch.Pooch` instance that is used to download the data.
+
+    Set's the cache location to the OS default plus ``ensaio/v1`` and can be
+    set manually using the ``ENSAIO_V1_DATA_DIR`` environment variable.
+
+    The download URL can be set using the ``ENSAIO_V1_URL`` environment
+    variable (default is ``doi:10.5281/zenodo.5167357``).
+
+    Returns
+    -------
+    repository : pooch.Pooch
+
+    """
+    url_var = "ENSAIO_V1_URL"
+    if url_var in os.environ and os.environ[url_var]:
+        url = os.environ[url_var]
+    else:
+        url = f"doi:{DOI}"
+    if not url.endswith("/"):
+        url = url + "/"
+    repository = pooch.create(
+        path=Path(pooch.os_cache("ensaio")) / "v1",
+        base_url=url,
+        registry={
+            "alps-gps-velocity.csv.xz": "md5:195ee3d88783ce01b6190c2af89f2b14",
+            "britain-magnetic.csv.xz": "md5:8dbbda02c7e74f63adc461909358f056",
+            "british-columbia-lidar.csv.xz": "md5:354c725a95036bd8340bc14e043ece5a",
+            "caribbean-bathymetry.csv.xz": "md5:a7332aa6e69c77d49d7fb54b764caa82",
+            "earth-geoid-10arcmin.nc": "md5:39b97344e704eb68fa381df2eb47da0f",
+            "earth-gravity-10arcmin.nc": "md5:56df20e0e67e28ebe4739a2f0357c4a6",
+            "earth-topography-10arcmin.nc": "md5:c43b61322e03669c4313ba3d9a58028d",
+            "southern-africa-gravity.csv.xz": "md5:1dee324a14e647855366d6eb01a1ef35",
+        },
+        # The name of an environment variable that can overwrite the path
+        env="ENSAIO_V1_DATA_DIR",
+    )
+    return repository
 
 
 def fetch_alps_gps():
     """
-    Fetch the Alpine 3-component GPS velocity dataset
+    Alpine 3-component GPS velocity dataset
 
     This is a compilation of 3D GPS velocities for the Alps. The horizontal
     velocities are reference to the Eurasian frame. All velocity components and
@@ -59,12 +84,13 @@ def fetch_alps_gps():
         Path to the downloaded file on disk.
 
     """
-    return Path(_REPOSITORY.fetch("alps-gps-velocity.csv.xz"))
+    repository = _make_repository()
+    return Path(repository.fetch("alps-gps-velocity.csv.xz"))
 
 
 def fetch_britain_magnetic():
     """
-    Fetch the digitized airborne magnetic survey of Britain
+    Digitized airborne magnetic survey of Britain
 
     This is a digitization of an airborne magnetic survey of Britain. Data are
     sampled where flight lines crossed contours on the archive maps. Contains
@@ -97,12 +123,13 @@ def fetch_britain_magnetic():
         Path to the downloaded file on disk.
 
     """
-    return Path(_REPOSITORY.fetch("britain-magnetic.csv.xz"))
+    repository = _make_repository()
+    return Path(repository.fetch("britain-magnetic.csv.xz"))
 
 
 def fetch_british_columbia_lidar():
     """
-    Fetch lidar point cloud data of the Trail Islands in BC, Canada
+    Lidar point cloud data of the Trail Islands in BC, Canada
 
     This is a lidar point cloud (ground reflections only) sliced to the small
     `Trail Islands <https://apps.gov.bc.ca/pub/bcgnws/names/21973.html>`__
@@ -129,4 +156,5 @@ def fetch_british_columbia_lidar():
         Path to the downloaded file on disk.
 
     """
-    return Path(_REPOSITORY.fetch("british-columbia-lidar.csv.xz"))
+    repository = _make_repository()
+    return Path(repository.fetch("british-columbia-lidar.csv.xz"))
