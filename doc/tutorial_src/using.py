39eaf5235ff13aa98c6a4b37708344965498e94b
Leonardo Uieda <leouieda@gmail.com>
2021-12-23 09:50:48 +0000

    Finish the intro tutorial

--- doc/tutorial_src/using.py
+++ doc/tutorial_src/using.py
@@ -8,51 +8,73 @@
 .. _using:
 
 Downloading data
-----------------
+================
 
-The location of the cache folder varies by operating system. Use the
-:func:`ensaio.v1.cache_folder` function to get its location. You can also set
-the location manually by creating a ``ENSAIO_V1_DATA_DIR`` environment variable
-with the desired path. Ensaio will search for this variable and if found will
-use its value instead of the default cache folder.
+Ensaio provides functions for downloading datasets from the `fatiando/data
+<https://github.com/fatiando/data>`__ collection to your computer. The
+functions are available through different modules for each major release of
+the data collection. For example, datasets from the version 1.X series are
+available through :mod:`ensaio.v1`.
 
+The recommended way to use Ensaio is to import a particular version module like
+so:
 """
+# Load Pandas as well so we can read in some data
 import pandas as pd
-import pygmt
 
 import ensaio.v1 as ensaio
 
 ###############################################################################
-# Download and cache the data and return the path to it on disk
+# To download a particular dataset, say our Southern Africa gravity data,
+# call the corresponding ``fetch_`` functions:
 fname = ensaio.fetch_southern_africa_gravity()
 print(fname)
 
 ###############################################################################
-# Load the CSV formatted data with pandas
+# .. tip::
+#
+#     You can browse a list of all available datasets in :ref:`api` or
+#     :ref:`gallery_v1`.
+#
+# If the data are not yet available on your computer, Ensaio will automatically
+# download it and return the path to the downloaded file.
+# In the file had already been downloaded, Ensaio won't repeat the download and
+# will only return the path to the existing file.
+#
+# This means that placing the code above in a Python script or Jupyter notebook
+# will mean that whoever runs it is guaranteed to get the data on their
+# computer.
+# Running the code multiple times or using the same data in multiple places
+# will only trigger a single download, saving bandwidth and storage space.
+#
+# .. note::
+#
+#     Ensaio uses `Pooch <https://www.fatiando.org/pooch/>`__ under the hood to
+#     make all of this work.
+#
+# Once we have the path to the data file, we can load it like we would any
+# other data file. In this case, our data is in a CSV file so the natural
+# choice is to use `Pandas <https://pandas.pydata.org/>`__:
 data = pd.read_csv(fname)
 data
 
 ###############################################################################
-# Make a PyGMT map with the data points colored by the gravity data.
-fig = pygmt.Figure()
-fig.basemap(
-    region=[
-        data.longitude.min() - 1,
-        data.longitude.max() + 1,
-        data.latitude.min() - 1,
-        data.latitude.max() + 1,
-    ],
-    projection="M15c",
-    frame=True,
-)
-pygmt.makecpt(cmap="viridis", series=[data.gravity_mgal.min(), data.gravity_mgal.max()])
-fig.plot(
-    x=data.longitude,
-    y=data.latitude,
-    color=data.gravity_mgal,
-    cmap=True,
-    style="c0.05c",
-)
-fig.colorbar(frame='af+l"gravity [mGal]"')
-fig.coast(shorelines=True, water="royalblue4", area_thresh=1e4)
-fig.show()
+# .. admonition:: Using Ensaio in your project documentation?
+#
+#     Make sure you take a look at :ref:`developers` for useful tips and
+#     tricks.
+#
+# Where are the data?
+# -------------------
+#
+# The location of the cache folder varies by operating system. Use the
+# :func:`ensaio.v1.cache_folder` function to get its location on your computer.
+print(ensaio.cache_folder())
+
+###############################################################################
+# You can also set the location manually by creating a ``ENSAIO_V1_DATA_DIR``
+# environment variable with the desired path. Ensaio will search for this
+# variable and if found will use its value instead of the default cache folder.
+#
+# Similar variables and functions are available for each data collection
+# version.
