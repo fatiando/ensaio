16077a4d398da4eaf7d5c178057e99bdc8bbce88
Leonardo Uieda <leouieda@gmail.com>
2022-10-21 15:13:11 +0100

    Remove trailing whitespace from gallery script

--- doc/gallery_src/lightning-creek-magnetic.py
+++ doc/gallery_src/lightning-creek-magnetic.py
@@ -18,7 +18,7 @@ Inlier, QLD (P1029), magnetic line data, AWAGS levelled. Geoscience Australia,
 Canberra <http://pid.geoscience.gov.au/dataset/ga/142419>`__
 
 **Pre-processing:** `Source code for preparation of original dataset
-for redistribution in Ensaio 
+for redistribution in Ensaio
 <https://github.com/fatiando-data/lightning-creek-magnetic-grid>`__
 
 """
